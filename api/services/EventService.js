ec05bb143aa5f46e7d39dc96789fa7604ffd0c23
Philipp Möhler <p.moehler@zebresel.com>
2015-01-31 14:33:05 +0100

    reconnect event implemented

--- api/services/EventService.js
+++ api/services/EventService.js
@@ -12,6 +12,7 @@ module.exports = {
      */
     
     USER_CREATED            : 'user:create',
-    USER_DISCONNECTED       : 'user:disconnect'
+    USER_DISCONNECTED       : 'user:disconnect',
+    USER_RECONNECTED        : 'user:reconnect'
     
 };
