09cf936c8970691e47de5e953eb0845b24a0ce12
Philipp Möhler <p.moehler@zebresel.com>
2015-01-31 18:57:23 +0100

    changed populate for command

--- api/services/CommandService.js
+++ api/services/CommandService.js
@@ -3,19 +3,29 @@ module.exports = {
     
     publish: function (message) {
 
-        User.findOrCreate({isServer: true, username: ConfigService.SERVER_USERNAME}, {isServer: true, username: ConfigService.SERVER_USERNAME}).populate('from').exec(function (error, serveruser) {
+        User.findOrCreate({isServer: true, username: ConfigService.SERVER_USERNAME}, {isServer: true, username: ConfigService.SERVER_USERNAME}).exec(function (error, serveruser) {
             
             if(!error) {
-                
-                Message.create({text: message, from: serveruser.id}).populate('from').exec(function(error, message) {
+
+                Message.create({text: message, from: serveruser.id}).exec(function(error, message) {
                     
                     if(error) {
                         
-                        console.log('can not create command message');
+                        return res.badRequest(error);
                     }
                     else {
 
-                        sails.sockets.emit(SessionService.getUserSockets(), EventService.MESSAGE_CREATED, message);
+                        Message.findOne({id: message.id}).populate('from').exec(function(error, populatedMessage) {
+                            
+                            if(error) {
+                                
+                                console.log('can not create command message');
+                            }
+                            else {
+                                
+                                sails.sockets.emit(SessionService.getUserSockets(), EventService.MESSAGE_CREATED, populatedMessage);
+                            }
+                        });
                     }
                 });
             }
